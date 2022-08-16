dd2b899b84f184c382dff8451779e7aa309cf9eb
Nadezhda <7_nadezhda@mail.ru>
2022-08-16 20:25:10 +0400

    del f

--- src/utils/capitalize.js
+++
@@ -1,7 +0,0 @@
-// сделать букву в названии города заглавной
-export const capitalize = (str) => {
-  const stringArray = str.split(/([\s-(])/g);
-  return stringArray
-    .map((el) => el.charAt(0).toUpperCase() + el.slice(1))
-    .join("");
-};
